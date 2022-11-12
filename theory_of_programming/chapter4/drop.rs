b46c0e05b3ea8890b260000068b66935de10091c
porterlu <1258210724@qq.com>
2022-11-12 16:31:11 +0800

    chapter 4

---
+++ theory_of_programming/chapter4/drop.rs
@@ -0,0 +1,15 @@
+use std::ops::Drop;
+#[derive(Debug)]
+struct S(i32);
+impl Drop for S{
+    fn drop(&mut self){
+        println!("drop {}", self.0);
+    }
+}
+
+fn main(){
+    let x = S(1);
+    {
+        let y = S(2);
+    }
+}
---
+++ theory_of_programming/chapter4/string_and_vec.rs
@@ -0,0 +1,4 @@
+fn main(){
+    let s = String::from("hello");
+    let v = vec![1, 2, 3];
+}
